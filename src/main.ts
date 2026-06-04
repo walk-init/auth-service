e4a7d96065298ad578e76382873c75a87a02815c
walkcat <wildblade207@yandex.ru>
2026-06-04 15:49:29 +0300

    auth,  but npm package not publish

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'auth.v1',
+      protoPath: 'node_modules/@walkcat/contracts/proto/auth.proto',
+      url: "localhost:50051",
+      loader: {
+        keepCase: false,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.init();
 }
 bootstrap();
---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { AuthService } from './auth.service';
+
+import type { SendOTPRequest, SendOTPResponse } from "@walkcat/contracts/gen/auth";
+
+@Controller()
+export class AuthController {
+  public constructor(private readonly authService: AuthService) {}
+
+  @GrpcMethod("AuthService", "SendOTP")
+  public async sendOtp(request: SendOTPRequest): Promise<SendOTPResponse> {
+    console.log(request);
+    return {
+      ok: true,
+    };
+  }
+}
